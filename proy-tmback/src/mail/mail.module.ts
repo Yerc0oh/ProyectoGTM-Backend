642b66ce468a8739f4393bd9b46fb732205942bd
Yerco <abedcalderonq@gmail.com>
2026-06-09 04:14:02 -0400

    fix: faltaba el controller del email en su module

--- proy-tmback/src/mail/mail.module.ts
+++ proy-tmback/src/mail/mail.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 
 @Module({
+  controllers: [MailController],
   providers: [MailService],
   exports: [MailService],
 })

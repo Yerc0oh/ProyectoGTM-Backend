07a509ec4b147fb1647e65c002627ac8a363468e
Yerco <abedcalderonq@gmail.com>
2026-06-09 04:01:20 -0400

    fix: mailmodule  no estaba en turnos module

--- proy-tmback/src/turnos/turnos.module.ts
+++ proy-tmback/src/turnos/turnos.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TurnosService } from './turnos.service';
 import { TurnosController } from './turnos.controller';
 import { MailService } from '../mail/resend.service';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [TurnosController],
   providers: [TurnosService, MailService],
 })

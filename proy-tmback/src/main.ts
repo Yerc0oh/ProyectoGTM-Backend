6eddad00c8b83cc85b94dbd7768294a36fa41a5f
Yerco <abedcalderonq@gmail.com>
2026-06-09 00:10:50 -0400

    fix: doble listen en el deployment

--- proy-tmback/src/main.ts
+++ proy-tmback/src/main.ts
@@ -6,8 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -15,15 +13,17 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: ['http://localhost:5173', process.env.FRONTEND_URL], 
+    origin: ['http://localhost:5173', process.env.FRONTEND_URL],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
-
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
 }
 bootstrap();
 
